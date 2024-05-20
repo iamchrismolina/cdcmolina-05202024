0807f9ebf5d4e8d8bf2e434e6e2b8c2cccd874f9
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-20 14:15:28 +0800

    feat: added dynamic text element container

---
+++ src/components/atoms/VText.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+const props = defineProps<{
+	tag: string,
+	content: string
+}>()
+
+function isTag = () => {
+	['p', 'label', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'].find((t) => t === tag.toLowerCase())
+}
+
+</script>
+<template>
+<component :is='isTag' v-bind='$attrs'>{{ content }}</component>
+
+</template>
+<style scoped></style>
