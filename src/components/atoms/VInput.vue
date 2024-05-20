6b590e78df5f75d332018e19061bc6a9f718e5cb
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-20 14:13:47 +0800

    feat: added dynamic input element

---
+++ src/components/atoms/VInput.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+const inputValue = defineModel('inputValue')
+
+const props = defineProps<{
+	inputType: string,
+}>()
+</script>
+
+<template>
+	<input :type='inputType' v-model='inputValue' v-bind="$attrs" />
+</template>
+
+<style scoped></style>
