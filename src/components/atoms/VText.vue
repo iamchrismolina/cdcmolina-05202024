407efbcd08952ff55dee8f24558c1c72d40c6be8
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-20 14:31:25 +0800

    feat: added form with label

--- src/components/atoms/VText.vue
+++ src/components/atoms/VText.vue
@@ -4,8 +4,8 @@ const props = defineProps<{
 	content: string
 }>()
 
-function isTag = () => {
-	['p', 'label', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'].find((t) => t === tag.toLowerCase())
+function isTag() {
+	return ['p', 'label', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'].find((t) => t === props.tag.toLowerCase())
 }
 
 </script>
--- src/components/pages/HomeView.vue
+++ src/components/pages/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup lang='ts'>
 import { useTasksStore } from '@/stores/tasks.ts'
 const { tasks, createTask } = useTasksStore()
+import VButton from '@/components/atoms/VButton.vue'
+import VInput from '@/components/atoms/VInput.vue'
+import VText from '@/components/atoms/VText.vue'
+import VTaskFrom from '@/components/molecules/VTaskForm.vue'
 
 </script>
 <template>
 	Test
 	{{ tasks }}
+	<VTaskForm>
+		<VText for='name' :tag="'label'" :content="'Name'" />
+	</VTaskForm>
 </template>
 <style scoped></style>
